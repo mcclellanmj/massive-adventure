55a2922c6d7477d378c2b2e7fa23eb69484b4057
Matt McClellan <mcclellan.mj@gmail.com>
2013-03-05 19:25:32 -0600

    Setting up libgdx project

---
+++ html/src/main/java/com/mcclellan/html/MainHtml.java
@@ -0,0 +1,19 @@
+package com.mcclellan.html;
+
+import com.mcclellan.core.Main;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class MainHtml extends GwtApplication {
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new Main();
+	}
+	
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		return new GwtApplicationConfiguration(480, 320);
+	}
+}
