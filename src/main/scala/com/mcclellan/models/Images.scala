c4e7682149b1358214b1d6b0bb71281863ffa557
Matt McClellan <mcclellan.mj@gmail.com>
2013-03-31 08:30:19 -0500

    Cleared out old files for libGDX conversion

--- src/main/scala/com/mcclellan/models/Images.scala
+++
@@ -1,8 +0,0 @@
-package com.mcclellan.models
-
-import java.io.File
-import org.newdawn.slick.Image
-
-object Images {
-	lazy val player = new Image("images/Player.png");
-}
--- src/main/scala/com/mcclellan/models/Position.scala
+++
@@ -1,9 +0,0 @@
-package com.mcclellan.models
-
-trait Position {
-  var position: Vector2f
-}
-
-trait Moving {
-  var acceleration: Vector2f
-}
