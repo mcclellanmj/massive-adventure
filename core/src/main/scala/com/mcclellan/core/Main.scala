9e79c32924347e614072e9d6882b90ff9bb6972f
Matt McClellan <mcclellan.mj@gmail.com>
2013-04-04 22:25:49 -0500

    Converted some of main to use Vector instead of floats

--- core/src/main/scala/com/mcclellan/core/Main.scala
+++ core/src/main/scala/com/mcclellan/core/Main.scala
@@ -11,6 +11,8 @@ import com.mcclellan.input.UserInputListener
 import com.mcclellan.input.Action
 import com.mcclellan.input.actions._
 import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.mcclellan.core.model.Player
+import com.mcclellan.core.math.Vector2
 
 class Main(val processor : MappedInputProcessor) extends ApplicationListener with UserInputListener {
 	import language.implicitConversions
@@ -18,10 +20,8 @@ class Main(val processor : MappedInputProcessor) extends ApplicationListener wit
 
 	var texture : Texture = null
 	var batch : SpriteBatch = null
-	var x = 100.0f;
-	var y = 100.0f;
-	var velocityUp = 0;
-	var velocityRight = 0;
+	val player = new Player(new Vector2(0f, 0f))
+	var direction = new Vector2(0f, 0f)
 
 	override def create = {
 		texture = new Texture(Gdx.files.classpath("Man.png"));
@@ -31,8 +31,7 @@ class Main(val processor : MappedInputProcessor) extends ApplicationListener wit
 	}
 
 	private def update(elapsed : Float) = {
-		y += (velocityUp * elapsed)
-		x += (velocityRight * elapsed)
+	  player.position += (direction * 150) * elapsed
 	}
 	
 	override def render = {
@@ -43,7 +42,7 @@ class Main(val processor : MappedInputProcessor) extends ApplicationListener wit
 		
 		batch.begin
 		font.draw(batch, String.valueOf(Gdx.graphics.getFramesPerSecond), 10, Gdx.graphics.getHeight - 20)
-		batch.draw(texture, x, y)
+		batch.draw(texture, player.position.x, player.position.y)
 		batch.end
 	}
 
@@ -53,13 +52,11 @@ class Main(val processor : MappedInputProcessor) extends ApplicationListener wit
 	override def dispose = Unit
 
 	override def userAction(action : Action) = {
-		println(action)
-
 		action match {
-			case Down(s) => velocityUp += -100 * !s
-			case Up(s) => velocityUp += 100 * !s
-			case Left(s) => velocityRight += -100 * !s
-			case Right(s) => velocityRight += 100 * !s
+			case Down(s) => direction += new Vector2(0,-1 * !s)
+			case Up(s) => direction += new Vector2(0, 1 * !s)
+			case Left(s) => direction += new Vector2(-1 * !s, 0)
+			case Right(s) => direction += new Vector2(1 * !s,0)
 		}
 	}
 }
--- core/src/main/scala/com/mcclellan/core/model/Player.scala
+++ core/src/main/scala/com/mcclellan/core/model/Player.scala
@@ -3,6 +3,6 @@ package com.mcclellan.core.model
 import com.mcclellan.core.math.Vector2
 
 
-class Player(val position : Vector2[Float]) {
+class Player(var position : Vector2[Float]) {
 
 }
